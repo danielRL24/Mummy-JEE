09f345c72ef4b91f1a6afff908e1fc566567508f
Daniel Rodrigues Lourenço <daniel.24.rodrigues@gmail.com>
2017-03-02 17:09:17 +0100

    Add entities from database

---
+++ Mummy/src/java/src/entities/Participant.java
@@ -0,0 +1,114 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package src.entities;
+
+import java.io.Serializable;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ *
+ * @author daniel
+ */
+@Entity
+@Table(name = "participant")
+@XmlRootElement
+@NamedQueries({
+    @NamedQuery(name = "Participant.findAll", query = "SELECT p FROM Participant p")
+    , @NamedQuery(name = "Participant.findById", query = "SELECT p FROM Participant p WHERE p.id = :id")
+    , @NamedQuery(name = "Participant.findByObservator", query = "SELECT p FROM Participant p WHERE p.observator = :observator")})
+public class Participant implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Basic(optional = false)
+    @Column(name = "id")
+    private Integer id;
+    @Column(name = "observator")
+    private Boolean observator;
+    @JoinColumn(name = "id_task", referencedColumnName = "id")
+    @ManyToOne(optional = false)
+    private Task idTask;
+    @JoinColumn(name = "id_user", referencedColumnName = "id")
+    @ManyToOne(optional = false)
+    private User idUser;
+
+    public Participant() {
+    }
+
+    public Participant(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Boolean getObservator() {
+        return observator;
+    }
+
+    public void setObservator(Boolean observator) {
+        this.observator = observator;
+    }
+
+    public Task getIdTask() {
+        return idTask;
+    }
+
+    public void setIdTask(Task idTask) {
+        this.idTask = idTask;
+    }
+
+    public User getIdUser() {
+        return idUser;
+    }
+
+    public void setIdUser(User idUser) {
+        this.idUser = idUser;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Participant)) {
+            return false;
+        }
+        Participant other = (Participant) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "src.entities.Participant[ id=" + id + " ]";
+    }
+    
+}
