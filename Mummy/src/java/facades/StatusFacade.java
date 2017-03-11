1bf6d9cbb7f1baf441870f563e4d8a49bcb6562a
Daniel Rodrigues Lourenço <daniel.24.rodrigues@gmail.com>
2017-03-11 19:23:07 +0100

    New Project with Roles

---
+++ Mummy/src/java/facades/StatusFacade.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package facades;
+
+import entities.Status;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+/**
+ *
+ * @author daniel
+ */
+@Stateless
+public class StatusFacade extends AbstractFacade<Status> {
+
+    @PersistenceContext(unitName = "MummyPU")
+    private EntityManager em;
+
+    @Override
+    protected EntityManager getEntityManager() {
+        return em;
+    }
+
+    public StatusFacade() {
+        super(Status.class);
+    }
+    
+}
